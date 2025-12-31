074f3ae32af98b3784ec1f8b7f2505622cc9e996
sathishdevops38 <sathishdevops38@gmail.com>
2025-12-31 14:04:37 +0530

    Intype

--- 60-robo-eks/main.tf
+++ 60-robo-eks/main.tf
@@ -36,7 +36,7 @@ module "eks" {
     blue = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.large"]
+      instance_types = ["m7i-flex.large"]
       iam_role_additional_policies  = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
         amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
